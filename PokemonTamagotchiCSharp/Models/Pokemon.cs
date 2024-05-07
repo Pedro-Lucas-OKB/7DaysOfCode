ed6a03fe6446c65366a23d1d47ece3905ce3eb8c
Pedro Lucas <pedrogmobile2@gmail.com>
2024-05-07 12:49:42 -0300

    initial commit: day 1

---
+++ PokemonTamagotchiCSharp/Models/Pokemon.cs
@@ -0,0 +1,7 @@
+﻿namespace PokemonRequest.Models;
+
+public class Pokemon
+{
+    public string Name { get; set; } = default!;
+    public string Url { get; set; } = default!;
+}
---
+++ PokemonTamagotchiCSharp/Models/PokemonResponse.cs
@@ -0,0 +1,9 @@
+using PokemonRequest.Models;
+
+public class PokemonResponse
+{
+    public int count { get; set; }
+    public string Next { get; set; } = default!;
+    public string Previous { get; set; } = default!;
+    public List<Pokemon> Results { get; set; } = default!;
+}
